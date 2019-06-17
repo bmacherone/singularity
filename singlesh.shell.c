2654403eb08c77a512bb1eee3db5d2332c0c9753
bmacherone <bmacherone@users.noreply.github.com>
2019-06-17 15:49:18 -0400

    Add files via upload

---
+++ singlesh.shell.c
@@ -0,0 +1,8 @@
+// A C++ program that pauses screen at the end in Windows OS 
+#include <iostream> 
+using namespace std; 
+int main () 
+{ 
+	system("/usr/local/bin/singularity shell /usr/local/singularity/singlesh.sif"); 
+	return 0; 
+} 
